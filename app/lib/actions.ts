ebeb6617e978c6afd352d4529653977b987a32e9
fedotovvsevolod <sevas727@gmail.com>
2026-02-21 11:22:29 +0200

    add auth? only sign in

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 import { z } from 'zod';
 
@@ -116,4 +119,23 @@ export async function deleteInvoice(id: string) {
 
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
