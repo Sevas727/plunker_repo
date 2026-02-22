67d86894d901188823a5a8334cb2515925728b69
fedotovvsevolod <sevas727@gmail.com>
2026-02-22 09:17:15 +0200

    add favicon + meta data

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from 'next';
+
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
@@ -7,6 +9,10 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import {useDebouncedCallback} from "use-debounce";
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
 
 export default async function Page(props: {
     searchParams?: Promise<{
